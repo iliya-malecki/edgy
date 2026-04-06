33b3374ad986b695f6dc60aa8453fe3495a336b4
Iliya Malecki <iliyamalecki@gmail.com>
2026-04-07 01:20:34 +0200

    fix: document expectations

--- core/runtime_context.py
+++ core/runtime_context.py
@@ -13,5 +13,15 @@ class RuntimeContext(t.Generic[Input, Output], t.Protocol):
     raw client connections and other runtime dependencies.
     """
 
-    def unsafe_pub(self, topic: type[Output], data: t.Any) -> None: ...
-    def unsafe_sub(self, topic: type[Input]) -> t.AsyncIterable[t.Any]: ...
+    def unsafe_pub(self, topic: type[Output], data: t.Any) -> None:
+        f"""
+        Unsafe at the type level. It is implementors responsibility 
+        to validate that data is compatible with {Topic.__name__}.model
+        """
+        ...
+    def unsafe_sub(self, topic: type[Input]) -> t.AsyncIterable[t.Any]:
+        f"""
+        Unsafe at the type level. It is implementors responsibility 
+        to unsure that returned data is compatible with {Topic.__name__}.model
+        """
+        ...
