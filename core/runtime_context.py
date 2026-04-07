92c841c860b54c1f7362f0b32fbd2a361ebfde2e
Iliya Malecki <iliyamalecki@gmail.com>
2026-04-07 14:58:58 +0200

    fix: impl guards

--- core/runtime_context.py
+++ core/runtime_context.py
@@ -4,16 +4,30 @@ import pydantic
 from .topic import Topic
 
 
+# Input and Output must be kept contravariant
 class RuntimeContext[Input: Topic, Output: Topic]:
     """
     Runtime context holding allowed input and output topics,
     raw client connections and other runtime dependencies.
     """
 
-    def guard_input_topic(self, topic: type[Input]) -> bool: ...
-    def guard_output_topic(self, topic: type[Output]) -> bool: ...
+    def __init__(
+        self, allowed_input: set[type[Topic]], allowed_output: set[type[Topic]]
+    ) -> None:
+        self.allowed_input = allowed_input
+        self.allowed_output = allowed_output
 
-    def unsafe_pub[M: pydantic.BaseModel](self, topic: type[Topic[M]], data: M) -> None:
+    def guard_input_topic(self, topic: type[Input]) -> bool:
+        return topic in self.allowed_input
+
+    def guard_output_topic(self, topic: type[Output]) -> bool:
+        return topic in self.allowed_output
+
+    def unsafe_pub[M: pydantic.BaseModel](
+        self,
+        topic: type[Topic[M]],
+        data: M,
+    ) -> None:
         f"""
         Unsafe at the type level. It is implementors responsibility
         to validate that topic is in the allowed set of topics for this runtime context.
@@ -21,7 +35,10 @@ class RuntimeContext[Input: Topic, Output: Topic]:
         """
         ...
 
-    def unsafe_sub[M: pydantic.BaseModel](self, topic: type[Topic[M]]) -> t.AsyncIterable[M]:
+    def unsafe_sub[M: pydantic.BaseModel](
+        self,
+        topic: type[Topic[M]],
+    ) -> t.AsyncIterable[M]:
         f"""
         Unsafe at the type level. It is implementors responsibility
         to validate that topic is in the allowed set of topics for this runtime context
